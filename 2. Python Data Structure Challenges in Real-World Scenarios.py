468ef9d8c47f93d0b7ebe43bfa2cbb3f4e065621
Marko <markogidej@yahoo.com>
2024-04-01 14:20:09 -0400

    Answered Question 2

--- 2. Python Data Structure Challenges in Real-World Scenarios.py
+++ 2. Python Data Structure Challenges in Real-World Scenarios.py
@@ -1,1 +1,16 @@
 # Task 1: Library System Enhancement
+
+library = [("1984", "George Orwell"), ("Brave New World", "Aldous Huxley")]
+
+def add_book(library, title, author):
+    book_pair = (title, author,)
+    if book_pair not in library:
+        library.append(book_pair)
+        print(f"Added {title} by {author} to the library!")
+    else:
+        print(f"Looks like {title} by {author} is already in the library!")
+
+    print(library)
+
+add_book(library, "Dune", "Frank Herbert")
+add_book(library, "1984", "George Orwell")
