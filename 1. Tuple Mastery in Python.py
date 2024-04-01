74187eaa068cba0bf809e32c05902799cb518a5d
Marko <markogidej@yahoo.com>
2024-04-01 14:07:01 -0400

    Answered Question 1

--- 1. Tuple Mastery in Python.py
+++ 1. Tuple Mastery in Python.py
@@ -1,1 +1,11 @@
 # Task 1: Formatting Flight Itineraries
+
+itinerary_list = [("Alice", "New York", "London"), ("Bob", "Tokyo", "San Francisco")]
+
+def print_itineraries(itineraries):
+    count = 1
+    for itinerary in itinerary_list:
+        print(f"Itinerary {count}: {itinerary[0]} - From {itinerary[1]} to {itinerary[2]}")
+        count += 1
+
+print_itineraries(itinerary_list)
